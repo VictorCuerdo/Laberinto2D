2c549a71654d21c47e5883ab2b8e792f62f90b36
vcuerdo <vcuerdo@uninorte.edu.co>
2022-04-28 16:33:05 -0500

    Se configura pantalla de inicio
    Se le pone un soundtrack al juego, se cambia el background, y se configuran bien los botones.

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("Nivel1");
     }
     public void Quit()
     {
